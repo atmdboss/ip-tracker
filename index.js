b9d547a29bdc346de852d12f7396d286faae24da
Moses Amiedor <atmdboss@yahoo.com>
2020-10-30 12:31:00 +0200

    dynamic port

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.post('/search', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port on ${process.env.PORT}`);
 });
