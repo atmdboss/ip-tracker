41e2ea744a10ccfc81ae4446a928b293ec7bac22
Moses Amiedor <atmdboss@yahoo.com>
2020-10-30 10:34:21 +0200

    ipify init

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.use(express.static('./public'));
 app.use(express.json());
 
-app.post('/search', (req, res) => {
-  const { name, ip } = req.body;
-  res.status(200).send(Object.assign({}, { name, ip }));
+app.post('/search', async (req, res) => {
+  const { ip } = req.body;
+
+  try {
+    const { data } = await axios.get(
+      `https://geo.ipify.org/api/v1?apiKey=${process.env.API_KEY}&ipAddress=${ip}`
+    );
+    res.status(200).send(data);
+  } catch (error) {
+    console.log({ error: error.message });
+    res.send({ error: error.message });
+  }
 });
 
 app.listen(3000, () => {
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,33 @@
-fetch('http://localhost:3000/search', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    ip: '199.299.399.001',
-    name: 'smth',
-  }),
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const form = document.querySelector('form');
+const formInput = document.querySelector('form input');
+const address = document.querySelector('.details .address p');
+const locale = document.querySelector('.details .location p');
+const timezone = document.querySelector('.details .timezone p');
+const isp = document.querySelector('.details .isp p');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // add error handling for empty inputs
+});
+
+function init() {
+  fetch('http://localhost:3000/search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      ip: '',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => updateDetails(data));
+}
+function updateMap() {}
+function updateDetails(responseData) {
+  address.textContent = responseData.ip;
+  isp.textContent = responseData.isp;
+  timezone.textContent = `UTC ${responseData.location.timezone}`;
+  locale.textContent = `${responseData.location.city}, ${responseData.location.country}`;
+}
+init();
