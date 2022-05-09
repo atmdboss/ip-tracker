1c2fa01ece6f4e8e9b4f05a1ac5136b5ea4728f5
Moses Amiedor <atmdboss@yahoo.com>
2022-05-09 12:03:51 +0200

    replaced ip address tracker with ipdata package

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
-const app = express();
+const cors = require('cors');
+const IPData = require('ipdata').default;
 
+const app = express();
+const ipdata = new IPData(process.env.API_KEY);
+
+app.use(cors());
 app.use(express.static('./public'));
 app.use(express.json());
 
@@ -10,10 +14,8 @@ app.post('/search', async (req, res) => {
   const { ip } = req.body;
 
   try {
-    const { data } = await axios.get(
-      `https://geo.ipify.org/api/v1?apiKey=${process.env.API_KEY}&ipAddress=${ip}`
-    );
-    res.status(200).send(data);
+    const info = await ipdata.lookup(ip);
+    res.status(200).send(info);
   } catch (error) {
     console.log({ error: error.message });
     res.send({ error: error.message });
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@ const errorDiv = document.querySelector('.error');
 const address = document.querySelector('.details .address p');
 const locale = document.querySelector('.details .location p');
 const timezone = document.querySelector('.details .timezone p');
+const region = document.querySelector('.details .region p');
 const isp = document.querySelector('.details .isp p');
 const myMap = L.map('map');
 
@@ -11,31 +12,23 @@ form.addEventListener('submit', handleSubmit);
 
 async function init() {
   try {
-    const { data } = await axios.get(
-      'https://www.cloudflare.com/cdn-cgi/trace'
-    );
+    const { data } = await axios.get('https://www.cloudflare.com/cdn-cgi/trace');
 
     const ipAdd = data
       .split('\n')
       .find((el) => el.includes('ip'))
       .split('=')[1];
 
-    const response = await axios.post(
-      'https://ip-reveal.herokuapp.com/search',
-      {
-        ip: ipAdd,
-      }
-    );
+    const response = await axios.post('https://ip-reveal.herokuapp.com/search', {
+      ip: ipAdd,
+    });
 
     updateDetails(response.data);
     updateMap(response.data);
   } catch (error) {
-    const response = await axios.post(
-      'https://ip-reveal.herokuapp.com/search',
-      {
-        ip: '',
-      }
-    );
+    const response = await axios.post('https://ip-reveal.herokuapp.com/search', {
+      ip: '',
+    });
 
     updateDetails(response.data);
     updateMap(response.data);
@@ -43,23 +36,21 @@ async function init() {
 }
 
 function updateMap(responseData) {
-  myMap.setView([responseData.location.lat, responseData.location.lng], 15);
+  myMap.setView([responseData.latitude, responseData.longitude], 15);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 15,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 17,
   }).addTo(myMap);
 
-  L.marker([responseData.location.lat, responseData.location.lng])
-    .addTo(myMap)
-    .openPopup();
+  L.marker([responseData.latitude, responseData.longitude]).addTo(myMap).openPopup();
 }
 
 function updateDetails(responseData) {
   address.textContent = responseData.ip;
-  isp.textContent = responseData.isp;
-  timezone.textContent = `UTC ${responseData.location.timezone}`;
-  locale.textContent = `${responseData.location.city}, ${responseData.location.country}`;
+  isp.textContent = responseData.carrier?.name || responseData.company.name;
+  timezone.textContent = `UTC ${responseData.time_zone.offset}`;
+  locale.textContent = `${responseData.city}, ${responseData.country_name}`;
+  region.textContent = responseData.region;
 }
 
 async function handleSubmit(e) {
@@ -76,16 +67,12 @@ async function handleSubmit(e) {
     return;
   } else {
     // regex for ipv4 address
-    const isValid = ipAddress.match(
-      /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gm
-    );
+    const isValid = ipAddress.match(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gm);
     if (isValid) {
-      const { data } = await axios.post(
-        'https://ip-reveal.herokuapp.com/search',
-        {
-          ip: isValid[0],
-        }
-      );
+      const { data } = await axios.post('https://ip-reveal.herokuapp.com/search', {
+        ip: isValid[0],
+      });
+
       updateDetails(data);
       updateMap(data);
     } else {
